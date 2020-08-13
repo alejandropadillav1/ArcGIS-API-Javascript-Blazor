63d536c17000c944ba7358b81ffc2f33cd1fca60
Alejandro Padilla V <alejandro.padilla@tecna-ice.com>
2020-08-12 23:45:29 -0500

    Add project files.

---
+++ ArcGISJavascriptBlazor/wwwroot/js/script.js
@@ -0,0 +1,25 @@
+﻿
+
+
+function RenderMapa() {
+    require(["esri/Map", "esri/views/SceneView"], function (Map, SceneView) {
+        var map = new Map({
+            basemap: "topo-vector",
+            ground: "world-elevation" // show elevation
+        });
+
+        var view = new SceneView({
+            container: "viewDiv",
+            map: map,
+            camera: {
+                position: {
+                    // observation point
+                    x: -118.808,
+                    y: 33.961,
+                    z: 25000 // altitude in meters
+                },
+                tilt: 65 // perspective in degrees
+            }
+        });
+    });
+}
